1d2b333beeb849a91d5195ba73a479914416da93
zhangrong <tyouyou07@gmail.com>
2017-10-23 00:46:02 +0900

    wirte the components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+// eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
+import BusinessList from '../src/components/BusinessList/BusinessList.js'
+import SearchBar from '../src/components/SearchBar/SearchBar.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>ravenous</h1>
+        <BusinessList />
+        <SearchBar />
       </div>
     );
   }
